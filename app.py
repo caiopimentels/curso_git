8d09b3f074aad2c2e0a437dfb06bac51bc72cb03
Caio Lindao <caiomps@gmail.com>
2023-09-08 18:31:01 -0300

    Correcao da barra lateral e nome de alguns graficos

--- app.py
+++ app.py
@@ -11,76 +11,56 @@ st.set_page_config(layout="wide")
 def create_dataframe_section(df):
     st.title("XGB - Database")
     st.markdown("## Dashbord de acompanhamento de metricas")
+    st.markdown("---")
 
     st.sidebar.image('image.jpg')
     st.sidebar.markdown("### Xtreme Groovy Bikes")
     st.sidebar.markdown("Sua melhor moto, você encontra aqui")
-    year = st.sidebar.slider("Selecione o ano de fabricação",df.loc[:,'year'].min(),df.loc[:,'year'].max(),
+
+    year = st.sidebar.slider("Ano de fabricação",df.loc[:,'year'].min(),df.loc[:,'year'].max(),
                              value=[df.loc[:,'year'].min(),df.loc[:,'year'].max()])
 
-    km_rodado = st.sidebar.slider("Qual a kilometragem",value=[df.loc[:,'km_driven'].min(),df.loc[:,'km_driven'].max()])
+    km_rodado = st.sidebar.slider("Kilometragem",value=[df.loc[:,'km_driven'].min(),df.loc[:,'km_driven'].max()])
     
-    selecao = df['year'].isin(year)
+    selecao = df['year'].between(*year)
+    df = df.loc[selecao,:]
 
+    selecao = df['km_driven'].between(*km_rodado)
     df = df.loc[selecao,:]
     
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        st.metric("Nº de motos na base", df.shape[0])
+        st.metric("Nº de motos na base", df.shape[0],help="Com base no filtro aplicado")
     
     with col2:
-        st.metric("Modelo mais novo",df.loc[:,'year'].max())
+        st.metric("Modelo mais novo",df.loc[:,'year'].max(),help="Com base no filtro aplicado")
 
     with col3:
-        st.metric("Modelo mais velho",df.loc[:,'year'].min())
+        st.metric("Modelo mais velho",df.loc[:,'year'].min(),help="Com base no filtro aplicado")
     
     with col4:
-        st.metric("Média de kilometragem", "{:,}".format(int(df.loc[:,'km_driven'].mean())).replace(",",".")+" km")
+        st.metric("Média de kilometragem", "{:,}".format(int(df.loc[:,'km_driven'].mean())).replace(",",".")+" km",
+                  help="Com base no filtro aplicado")
 
-    # col_1.header("Database")
-    # col_1.dataframe(df, height=530)
-
-#    col_2.header("Data Description")
-
-#     data_description = """
-#                         | Coluna | Descrição |
-#                         | :----- | --------: |
-#                         | ID | Identificador da linha/registro |
-#                         | name | Fabricante e Modelo da Moto |
-#                         | selling_price | Preço de Venda |
-#                         | year | Ano de Fabricação da Moto |
-#                         | seller_type | Tipo de Vendedor - Se é vendedor pessoal ou revendedor |
-#                         | owner | Se é primeiro, segundo, terceiro ou quarto dono da moto |
-#                         | km_driven | Quantidade de Quilometros percorrido pela moto |
-#                         | ex_showroom_price | Preço da motocicleta sem as taxas de seguro e registro |
-#                         | age | Quantidade de anos em que a moto está em uso |
-#                         | km_class | Classificação das motos conforme a quilometragem percorrida |
-#                         | km_per_year | Quantidade de Quilometros percorridos a cada ano |
-#                         | km_per_month | Quantidade de Quilometros percorridos por mês |
-#                         | company | Fabricanete da Motocicleta |
-#     """
-
-#     col_2.markdown(data_description)
 def create_answers_section(df):
-    st.title("Main Questions Answers")
-
-    st.header("First Round")
+    st.markdown("---")
+    st.header("Gráficos")
     st.subheader(
-        "How many bikes are being sold by their owners and how many bikes are being sold by distributors?"
+        "Distribuição das motos anunciadas"
     )
     asw.rd1_question_9(df)
 
-    st.subheader("How many bikes are being sold are bikes from a unique owner?")
+    st.subheader("Anúncios de unico dono")
     asw.rd1_question_13(df)
 
     st.subheader(
-        "Are high kilometer bikes more expensive than bikes with lower kilometer?"
+        "Faixa de preço por kilometragem"
     )
     asw.rd1_question_14(df)
 
     st.subheader(
-        "Are the bikes with a unique owner more expense on avarege than the other bikes?"
+        "Faixa de preço por número de donos"
     )
     asw.rd2_question_1(df)
 
